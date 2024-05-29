8e726b4d33b31e50573636538e25b5c635c1fbec
Ethan A <158523809+EthanFAndrews@users.noreply.github.com>
2024-05-29 13:56:22 +1000

    Update LearningArduino.ino
    added flowing lights

--- LearningArduino.ino
+++ LearningArduino.ino
@@ -1,15 +1,68 @@
 static unsigned int myNum = 8;
-
+static unsigned int myNum2 = 9;
+static unsigned int myNum3 = 10;
+static unsigned int myNum4 = 11;
+static unsigned int myNum5 = 12;
+static unsigned int myNum6 = 13;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(myNum, OUTPUT);
+  pinMode(myNum2, OUTPUT);
+  pinMode(myNum3, OUTPUT);
+  pinMode(myNum4, OUTPUT);
+  pinMode(myNum5, OUTPUT);
+  pinMode(myNum6, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(myNum, HIGH);
-  delay(1000);
+  digitalWrite(myNum, true);
+  delay(100);
+  digitalWrite(myNum2, true);
+  delay(100);
+  digitalWrite(myNum3, true);
+  delay(100);
+  digitalWrite(myNum4, true);
+  delay(100);
+  digitalWrite(myNum5, true);
+  delay(100);
+  digitalWrite(myNum6, true);
+  delay(100);
   digitalWrite(myNum, false);
-  delay(500);
+  delay(100);
+  digitalWrite(myNum2, false);
+  delay(100);
+  digitalWrite(myNum3, false);
+  delay(100);
+  digitalWrite(myNum4, false);
+  delay(100);
+  digitalWrite(myNum5, false);
+  delay(100);
+  digitalWrite(myNum6, false);
+  delay(100);
+  digitalWrite(myNum6, true);
+  delay(100);
+  digitalWrite(myNum5, true);
+  delay(100);
+  digitalWrite(myNum4, true);
+  delay(100);
+  digitalWrite(myNum3, true);
+  delay(100);
+  digitalWrite(myNum2, true);
+  delay(100);
+  digitalWrite(myNum, true);
+  delay(100);
+  digitalWrite(myNum6, false);
+  delay(100);
+  digitalWrite(myNum5, false);
+  delay(100);
+  digitalWrite(myNum4, false);
+  delay(100);
+  digitalWrite(myNum3, false);
+  delay(100);
+  digitalWrite(myNum2, false);
+  delay(100);
+  digitalWrite(myNum, false);
+  delay(100);
 }
