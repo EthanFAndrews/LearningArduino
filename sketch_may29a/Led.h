9a9ecfa60a8d407ec5d400a91932b9306e3ab691
Ethan A <ethan.andrews14@education.nsw.gov.au>
2024-06-04 15:02:05 +1000

    Finished LED class

--- sketch_may29a/Led.h
+++ sketch_may29a/Led.h
@@ -9,37 +9,13 @@ private:
   byte pin;
 public:
   Led() {} // do not use
+  Led(byte pin);
 
-  Led(byte pin)
-  {
-    this->pin = pin;
-  }
+  void init();
+  void init(byte defaultState);
 
-  void init()
-  {
-    pinMode(pin, OUTPUT);
-  }
-
-  void init(byte defaultState)
-  {
-    init();
-    if (defaultState == HIGH) {
-      on();
-    }
-    else {
-      off();
-    }
-  }
-
-  void on()
-  {
-    digitalWrite(pin, HIGH);
-  }
-
-  void off()
-  {
-    digitalWrite(pin, LOW);
-  }
+  void on();
+  void off();
 };
 
 #endif
