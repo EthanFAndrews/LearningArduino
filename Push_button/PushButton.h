2bab5738fd49debf03dbfdfffc99022e9fb8468a
Ethan A <158523809+EthanFAndrews@users.noreply.github.com>
2024-06-05 09:03:33 +1000

    nearly finished pushbutton

---
+++ Push_button/PushButton.h
@@ -0,0 +1,23 @@
+#ifndef PUSH_BUTTON_H
+#define PUSH_BUTTON_H
+
+#include <Arduino.h>
+
+class PushButton
+{
+  private:
+    byte pin;
+    byte state;
+
+    bool isPullUp;
+    bool internalPullUpActivated;
+
+  public:
+    PushButton() {};
+    PushButton(byte pin, bool isPullUp, bool internalPullUpActivated)
+
+    void init();
+    byte readState();
+
+    bool isPressed();
+}
---
+++ Push_button/Push_button.ino
@@ -0,0 +1,17 @@
+
+#define BUTTON_Pin 2
+
+PushButton(BUTTON_PIN, true, true);
+
+void setup() {
+  Serial.begin(115200);
+  button.init();
+  
+
+}
+
+void loop() {
+  Serial.println(button.readState());
+  delay(100);
+
+}
